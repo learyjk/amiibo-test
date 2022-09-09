59e2c60e6dbbcb1091edaf64242f37d4bbffe041
Keegan Leary <leary.keegan@gmail.com>
2022-09-09 15:25:13 -0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import { greetUser } from '$utils/greet';
+window.fsAttributes = window.fsAttributes || [];
+window.fsAttributes.push([
+  'cmsfilter',
+  (filterInstances) => {
+    console.log('cmsfilter Successfully loaded!');
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+    // The callback passes a `filterInstances` array with all the `CMSFilters` instances on the page.
+    const [filterInstance] = filterInstances;
+    console.log(filterInstances);
+
+    // The `renderitems` event runs whenever the list renders items after filtering.
+    filterInstance.listInstance.on('renderitems', (renderedItems) => {
+      console.log(renderedItems);
+    });
+  },
+]);
